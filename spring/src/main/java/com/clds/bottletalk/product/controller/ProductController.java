e7ebf035cba63839d82c686d5e7adf8507a73bdd
daback <daback93@gmail.com>
2024-07-16 16:54:31 +0900

    Fix: ProductController.java의 Slf4j 로그를 JSONString으로 출력하고 로그 적재

--- spring/src/main/java/com/clds/bottletalk/product/controller/ProductController.java
+++ spring/src/main/java/com/clds/bottletalk/product/controller/ProductController.java
@@ -2,13 +2,12 @@ package com.clds.bottletalk.product.controller;
 
 
 import com.clds.bottletalk.common.Criteria;
-import com.clds.bottletalk.common.PageDTO;
 import com.clds.bottletalk.common.PagingResponseDTO;
 import com.clds.bottletalk.common.ResponseDTO;
 import com.clds.bottletalk.product.model.ProductDTO;
 import com.clds.bottletalk.product.service.ProductService;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
-import java.time.LocalDateTime;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -18,10 +17,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
-
-
 
 @Controller
 @RequestMapping("/v1/api/products")
@@ -38,16 +33,13 @@ public class ProductController {
     public ResponseEntity<ResponseDTO> getProduct(@PathVariable("productId") long productId){
         ProductDTO productDTO = productService.findProductByProductId(productId);
 
-
-
-        log.info("selected product name => {}", productDTO.getName());
-        log.info("selected product DTO => {}", productDTO.toString());
-
+        Gson gson = new Gson();
+        // Gson을 활용해 productDTO -> JSON String
+        String productJson = gson.toJson(productDTO);
+        log.info(productJson);
 
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"조회성공",productDTO));
-
     }
-
 
     @GetMapping()
     public ResponseEntity<ResponseDTO> getProductListWithPaging(@RequestParam(name = "search", defaultValue = "") String search,
@@ -57,24 +49,11 @@ public class ProductController {
         Page<ProductDTO> productDTOList = productService.findProductListWithPaging(cri,search);
         pagingResponseDTO.setData(productDTOList);
 
-        log.info("searched Keyword => {}  " , search);
-
+        // searchKeyword가 있는 경우, keyword를 log로 출력
+        if(search != null && !search.isEmpty()){
+            String searchJson = "{ \"searchKeyword\" : \""+search+"\" }";
+            log.info(searchJson);
+        }
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"조회성공",pagingResponseDTO));
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
